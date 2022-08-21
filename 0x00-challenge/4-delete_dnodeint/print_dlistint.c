8de632cfa782937a87a347d9080ab416eb9d7848
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-21 23:58:53 +0000

    print

--- 0x00-challenge/4-delete_dnodeint/print_dlistint.c
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -1,7 +1,23 @@
-#!/usr/bin/python3
+#include <stdio.h>
+#include "lists.h"
 
-""" FizzBuzz
+/**
+ * print_dlistint - Prints a doubly linkedlist of integers
+ *
+ * @h: A pointer to the first element of a list
+ *
+ * Return: The number of element printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
 
-"""
-
-import sys
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
